10d51e1a86727f6dd6efb837f87415ef8b07d94c
Jack <78120697+Jack-2077@users.noreply.github.com>
2022-05-16 19:43:25 -0700

    fix: router query param returning undefined

--- pages/work/[reel].js
+++ pages/work/[reel].js
@@ -1,24 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import Image from 'next/image';
 import { REEL_IMAGES2 } from '../Assests_imports';
+import { Grid } from '@mui/material';
 
 export default function reel() {
-  const {
-    query: { reel },
-  } = useRouter();
+  const [reelInfo, setReelInfo] = useState();
 
-  const reelName = reel.replaceAll(' ', '');
-  const reelInfo = REEL_IMAGES2[reelName];
+  const router = useRouter();
+
+  useEffect(() => {
+    if (router.isReady) {
+      const { reel } = router.query;
+
+      const reelName = reel.replaceAll(' ', '');
+      const demoReel = REEL_IMAGES2[reelName];
+      setReelInfo(demoReel);
+    }
+  }, [router.isReady]);
 
   return (
-    <>
-      {reelInfo.set.main.map((item, i) => (
-        <Image src={item} key={i} />
+    <Grid container spacing={0}>
+      {reelInfo?.set.main?.map((item, i) => (
+        <Grid item xs={12} key={i}>
+          <Image src={item} layout='responsive' />
+        </Grid>
       ))}
-      {reelInfo.set.grey?.map((item, i) => (
-        <Image src={item} key={i} />
+      {reelInfo?.set.grey?.map((item, i) => (
+        <Grid item xs={12} key={i}>
+          <Image src={item} layout='responsive' />
+        </Grid>
       ))}
-    </>
+      {reelInfo?.set.video && (
+        <Grid item xs={12}>
+          <video autoPlay muted>
+            <source src={reelInfo.set.video} type='video/mp4' />
+            <Image src='/logo.png' height={20} width={20} />
+          </video>
+        </Grid>
+      )}
+    </Grid>
   );
 }
